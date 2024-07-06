1c5be51f6484a82c6728d5f32bc22b0fe9faf2a0
JorgeAlvarado256 <jorge.alvarado19@inacapmail.cl>
2024-07-06 12:56:36 -0400

    actualizacion de solicitante y jefatura con funcinalidad completas.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require("./app/app");
 
 // Puerto en el que se ejecutará el servidor
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor Express en funcionamiento en el puerto ${PORT}`);
