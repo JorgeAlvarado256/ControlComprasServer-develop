17e4b8d4050f5738d0d1f313b48507641280edd1
JorgeAlvarado256 <jorge.alvarado19@inacapmail.cl>
2024-07-07 23:29:57 -0400

    actualizacion de sequelize

--- config/db.sequelize.js
+++ config/db.sequelize.js
@@ -2,14 +2,23 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-    process.env.DB_DATABASE,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_DATABASE,  // Nombre de la base de datos
+    process.env.DB_USER,      // Usuario de la base de datos
+    process.env.DB_PASSWORD,  // Contraseña de la base de datos
     {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT, // Usamos DB_DIALECT en lugar de DB_MYSQL
-        port: process.env.DB_PORT,
+        host: process.env.DB_HOST,       // Host de la base de datos
+        dialect: process.env.DB_DIALECT, // Dialecto de la base de datos (por ejemplo, mysql)
+        port: process.env.DB_PORT,       // Puerto de la base de datos
     }
 );
+
+// Prueba de conexión
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 module.exports = sequelize;
